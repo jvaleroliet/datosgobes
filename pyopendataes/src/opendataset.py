d92dba9c75aec02c7ba9228178bd022c850e61c7
jvaleroach <jvalero@accioncontraelhambre.org>
2024-07-09 11:29:41 +0200

    added representation

--- pyopendataes/src/opendataset.py
+++ pyopendataes/src/opendataset.py
@@ -3,15 +3,18 @@ import numpy as np
 import requests
 
 
-class OpenDataset:
-    def __init__(self, url):
+class OpenDataSet:
+    def __init__(self, url:str):
         self.url = url
         self.id = url.split('/')[-1]
         self.metadata = {}
     
+    def __repr__(self):
+        return f"OpenDataSet('{self.id}')"
+    
         
     def _extract_from_meta(self, meta: dict) -> None:
-        self.metadata["id"] = meta["id"]
+        self.metadata["id"] = meta["_about"].split('/')[-1]
         self.metadata["about"] = meta["_about"]
         self.metadata["description"] = meta["description"]
         self.metadata["identifier"] = meta["identifier"]
