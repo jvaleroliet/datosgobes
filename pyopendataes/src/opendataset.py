8e5ff8b6cbd3c813be4a8f695d34593c55e844cf
jvaleroach <jvalero@accioncontraelhambre.org>
2024-07-16 09:36:34 +0200

    created first opendataset class

--- pyopendataes/src/opendataset.py
+++ pyopendataes/src/opendataset.py
@@ -7,24 +7,67 @@ class OpenDataSet:
     def __init__(self, url:str):
         self.url = url
         self.id = url.split('/')[-1]
-        self.metadata = {}
+        # self.metadata = {}
     
     def __repr__(self):
         return f"OpenDataSet('{self.id}')"
     
+    @property    
+    def metadata(self):
+        # Fetch metadata from the API
+        response = requests.get(f"http://datos.gob.es/apidata/catalog/dataset/{self.id}")
+        return response.json()["result"]["items"][0]
         
-    def _extract_from_meta(self, meta: dict) -> None:
-        self.metadata["id"] = meta["_about"].split('/')[-1]
-        self.metadata["about"] = meta["_about"]
-        self.metadata["description"] = meta["description"]
-        self.metadata["identifier"] = meta["identifier"]
-        self.metadata["issued"] = meta["issued"]
-        self.metadata["keyword"] = meta["keyword"]
-        self.metadata["language"] = meta["language"]
-        self.metadata["modified"] = meta["modified"]
-        self.metadata["publisher"] = meta["publisher"]
-        self.metadata["theme"] = meta["theme"]
-        self.metadata["title"] = meta["title"]
-        self.metadata["type"] = meta["type"]
 
-        
+    @property
+    def publisher_data_url(self):
+        return self.metadata.get('identifier')
+
+    @property
+    def title(self):
+        # Extract all title values based on language code
+        titles = {d['_lang']: d['_value'] for d in self.metadata.get('title', [])}
+        return titles
+
+    @property
+    def description(self):
+        # Extract the first description value for each language
+        descriptions = {d['_lang']: d['_value'] for d in self.metadata.get('description', [])}
+        return descriptions
+
+    @property
+    def keywords(self):
+        # Group keywords by language and remove duplicates
+        keywords = {}
+        for entry in self.metadata.get('keyword', []):
+            lang_code = entry['_lang']
+            keyword = entry['_value']
+            keywords.setdefault(lang_code, set()).add(keyword)
+        return {lang: list(values) for lang, values in keywords.items()}
+
+
+    def get_distribution_by_format(self, format):
+        """
+        Returns the distribution information for a specific format (e.g., application/geo+pdf).
+        """
+        for distribution in self.distributions:
+            if distribution['format']['value'] == format:
+                return distribution
+        return None
+    
+    @property
+    def distributions(self):
+        """
+        Returns a list of dictionaries, each containing information for a single distribution.
+        """
+        distributions = self.metadata.get('distribution', [])
+        info_list = []
+        for distribution in distributions:
+            info = {}
+            info['accessURL'] = distribution.get('accessURL')
+            info['byteSize'] = distribution.get('byteSize')
+            info['format'] = distribution.get('format')['value']  # Extract format value
+            # Extract title information for all languages efficiently
+            info["titles"] = {d['_lang']:d['_value'] for d in distribution.get('title', [])}
+            info_list.append(info)
+        return info_list
